b83b8cff10379d6e82f289c390fa8e46ef4c73ba
Stryukov <stryukov.semen@gmail.com>
2023-09-07 23:32:57 +1200

    fix: remove import ingredients

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -9,7 +9,7 @@ echo "PostgreSQL started"
 
 python manage.py collectstatic --no-input \
 && python manage.py migrate \
-&& python manage.py import_ingredients --file_path='ingredients.json' \
+# && python manage.py import_ingredients --file_path='ingredients.json' \
 && gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
 
 exec "$@"
